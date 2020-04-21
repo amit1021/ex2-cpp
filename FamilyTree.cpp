797d77b41b8628a5501854c274ac78a700573242
amit1021 <amitelbaz1021@gmail.com>
2020-04-21 22:44:28 +0300

    first commit

---
+++ FamilyTree.cpp
@@ -0,0 +1,45 @@
+# include "FamilyTree.hpp"
+using namespace family;
+
+
+Tree :: Tree(string name)
+{
+
+}
+
+Tree &Tree :: addFather(string chils, string father)
+{
+    
+}
+Tree &Tree :: addMother(string child, string mother)
+{
+
+}
+
+string Tree :: relation(string name)
+{
+
+}
+
+string Tree :: find(string name)
+{
+
+}
+
+void Tree :: display()
+{
+
+
+}
+void Tree :: remove(string name)
+{
+
+}
+int Tree :: relationHelp(Tree * T, string name, int level)
+{
+
+}
+int Tree :: findHelp(Tree * T,string name, int level)
+{
+
+}
